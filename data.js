88b4df2f6238aafd905b96a63fe22cc9ccf3edb0
saurabhjha004 <saurabhdkmjha@gmail.com>
2024-12-19 19:06:29 +0530

    Apply .gitignore to existing files

--- data.js
+++ data.js
@@ -1,5 +1,6 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const dbURI = process.env.MONGODB_URI;
 
--- server.js
+++ server.js
@@ -4,7 +4,16 @@ require('./data');
 
 
 app.get('/', (req, res)=>{
-    res.send("hello world");
+    res.send("hello mongo");
+});
+
+app.get('/api', (req, res)=>{
+    res.send("hello api");
+});
+
+app.get('/api/:name', (req, res)=>{
+    const { name } = req.params;
+    res.send("hello ${name}");
 });
 
 app.listen(5000, ()=>{
